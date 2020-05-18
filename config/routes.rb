64f496e4512733307c4ce5dae7620f05ef82704e
code404it <jessikahd.smith@gmail.com>
2020-05-18 07:50:16 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :accounts
+  get 'accounts/new'
+
+  get 'accounts/create'
+
+  get 'accounts/edit'
+
+  get 'accounts/update'
+
+  get 'accounts/destroy'
+
+  get 'accounts/index'
+
+  get 'accounts/show'
+
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
+  get 'about', to: 'static#about'
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
